44b8c60a574ec4b8984b26bb1b2621ac0416e941
Ebanusssssi <kolisnichenkov@icloud.com>
2025-02-17 23:53:58 +0000

    slider fix

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -4,13 +4,17 @@ import { useEffect, useState } from "react";
 const Carousel = ({images}) => {
   const [slide, setSlide] = useState(0);
 
-  // useEffect(() => {
-  //   slideChange()
-  // }, [])
+  useEffect(() => {
+    console.log('effect');
+    
+    const timer = setTimeout(() => {
+      console.log('timer');
+      
+      setSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
+    }, 7000); // Интервал переключения слайдов 7 секунд
 
-  setTimeout(() => {
-    setSlide(slide === images.length - 1 ? 0 : slide + 1);
-  }, 7000);
+    return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
+  }, [slide, images.length]); // Зависимость от слайда и длины массива изображений
 
   return (
     <div className="w-full h-full flex">
@@ -20,7 +24,7 @@ const Carousel = ({images}) => {
             key={index}
             src={item} 
             alt="Slide Image"
-            className={`min-w-full h-full object-cover object-center transform translate-x-[-${100 * slide}%] transition-all duration-1000 ease-in-out`}
+            className={`min-w-full h-full object-cover object-center transform translate-x-[-${slide * 100}%] transition-all duration-1000 ease-in-out`}
           />
         ))
       }
