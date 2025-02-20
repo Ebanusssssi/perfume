facb95abfd73640e582e52ad06a6d849e7f0f54c
Ebanusssssi <kolisnichenkov@icloud.com>
2025-02-20 01:07:59 +0000

    minor bugs fix

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <div className="pb-[22vw] lg:pb-0 w-full lg:w-2/4 h-full flex flex-col items-center justify-center lg:justify-center gap-[4vw] lg:gap-[2vw] z-10 text-white lg:text-black">
         <h2 className="max-w-[90%] lg:max-w-[70%] text-center leading-tight lg:leading-none text-[10vw] lg:text-[3vw] font-medium select-none">{HOME_HERO.title}</h2>
         <p className="max-w-[90%] lg:max-w-[80%] font-normal text-center leading-tight text-[5vw] tracking-wide lg:text-[1.5vw] select-none">{HOME_HERO.description}</p>
-        <div className="fixed bottom-[3.5vw] lg:bottom-0 lg:relative mt-[1.5vw]">
+        <div className="fixed bottom-[12vw] lg:bottom-0 lg:relative mt-[1.5vw]">
           <Link
             to="/products"
             className="font-normal lg:font-semibold text-[4vw] lg:text-[1vw] tracking-wide text-white bg-black border border-black hover:border-slate-200 
